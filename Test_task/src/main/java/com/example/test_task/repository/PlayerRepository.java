cb7ae2438c15fd6185196a463f797ab5112d3a6e
Sem4enko <74368102+Sem4enko@users.noreply.github.com>
2022-03-10 21:27:03 +0200

    Delete Test_task directory

--- Test_task/src/main/java/com/example/test_task/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test_task.repository;
-
-import com.example.test_task.entity.Player;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends CrudRepository<Player,Integer> {
-}
